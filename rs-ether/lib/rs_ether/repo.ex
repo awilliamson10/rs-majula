329f846e27f1b7b191bcd89c7fc416949cd2fdc5
Jordan Abraham <personal.jordanabraham@gmail.com>
2026-05-31 22:31:26 -0400

    chore: make rs-ether a submodule instead of embedding it directly

--- rs-ether/lib/rs_ether/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule RsEther.Repo do
-  use Ecto.Repo,
-    otp_app: :rs_ether,
-    adapter: Ecto.Adapters.Postgres
-end
--- rs-ether/test/test_helper.exs
+++
@@ -1,2 +0,0 @@
-ExUnit.start()
-Ecto.Adapters.SQL.Sandbox.mode(RsEther.Repo, :manual)
